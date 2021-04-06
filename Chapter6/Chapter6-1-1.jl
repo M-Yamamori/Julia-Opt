47d907b13384c36ad895471027ca030ce11eb3f8
Miwa-Y <78003547+Miwa-Y@users.noreply.github.com>
2021-04-06 10:45:17 +0900

    Update Chapter6-1-1.jl

--- Chapter6/Chapter6-1-1.jl
+++ Chapter6/Chapter6-1-1.jl
@@ -1,5 +1,6 @@
 using CSV, DataFrames
 
+#Data preparations
 network_data = CSV.read("simple_network.csv", DataFrame)
 network_data2 = CSV.read("simple_network_2.csv", DataFrame)
 
@@ -10,14 +11,17 @@ u = network_data[:, 4]
 
 b = network_data2[:, 2]
 
+#Assignments of number of nodes and number of links
 no_node = max(maximum(start_node), maximum(end_node))
 no_link = length(start_node)
 
+#Creat a graph
 nodes = 1:no_node
 links = Tuple((start_node[i], end_node[i]) for i in 1:no_link)
 c_dict = Dict(links .=> c)
 u_dict = Dict(links .=> u)
 
+#Including the module
 include("mcnf.jl")
 using Main.MCNF
 x_star, obj = MCNF.minimal_cost_network_flow(nodes, links, c_dict, u_dict, b)
