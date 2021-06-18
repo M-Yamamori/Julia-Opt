8959d38f2cec1b19eaa1d8dae3b0d8ea5d593612
Miwa-Y <m.yamamori1999@gmail.com>
2021-06-18 21:21:38 +0900

    Create Chapter7-3-1.jl

---
+++ Chapter7/Chapter7-3-1.jl
@@ -0,0 +1,11 @@
+using JuMP, Ipopt
+m = Model(Ipopt.Optimizer)
+
+@variable(m, x[1:2])
+@NLobjective(m, Min, (x[1]-3)^3 + (x[2]-4)^2)
+@NLconstraint(m, (x[1]-1)^2 + (x[2]+1)^3 + exp(-x[1]) <= 1)
+
+optimize!(m)
+
+println("Optimal objective function value = ", getobjectivevalue(m))
+println("Optimal solution = ", JuMP.value.(x))
