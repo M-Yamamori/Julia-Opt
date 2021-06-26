e51c3dda188771179db6a8a53d17a6e5675965b5
Miwa-Y <m.yamamori1999@gmail.com>
2021-06-26 10:54:57 +0900

    Update Chapter7-2.jl

--- Chapter7/Chapter7-2.jl
+++ Chapter7/Chapter7-2.jl
@@ -2,8 +2,8 @@ using JuMP, Ipopt
 m = Model(Ipopt.Optimizer)
 
 @variable(m, x[1:2])
-@NLobjective(m, Min, (x[1]-3)^2 + (x[2]-4)^2)
-@NLconstraint(m, (x[1]-1)^2 + (x[2]+1)^2 <= 1)
+@objective(m, Min, (x[1]-3)^2 + (x[2]-4)^2)
+@constraint(m, (x[1]-1)^2 + (x[2]+1)^2 <= 1)
 
 JuMP.optimize!(m)
 
